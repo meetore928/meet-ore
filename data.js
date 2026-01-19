1228672528e910aa803761b93c7adfc4314518b7
qmo221177-alt <qmo221177@gmail.com>
2026-01-19 11:36:05 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,42 @@
+// 這裡是用戶的商品資料庫
+// 你可以用記事本打開這個檔案修改
+// 圖片請放在 images 資料夾，路徑寫 "images/你的檔名.jpg"
+
+const products = [
+    {
+        category: "圓珠",
+        id: "圓珠01",
+        image: "images/bead01.jpg", // 請確保你有這張圖片，否則會顯示灰色圖塊
+        specs: "鏈條：1.2mm*60cm鋼鍊，主珠：2cm圓天珠，配珠：0.2cm白月光珠"
+    },
+    {
+        category: "圓珠",
+        id: "圓珠02",
+        image: "images/bead02.jpg",
+        specs: "鏈條：純銀復古鍊，主珠：1.5cm黑曜石，配珠：無，備註：表面有天然紋路"
+    },
+    {
+        category: "圓珠",
+        id: "圓珠03",
+        image: "images/bead03.jpg",
+        specs: "主珠：18mm紅瑪瑙，配珠：3mm金珠，鏈條：紅色編織繩，適合手圍：15-17cm"
+    },
+    {
+        category: "圓珠",
+        id: "圓珠04",
+        image: "images/bead04.jpg",
+        specs: "風格：藏式復古，主珠：老礦圓珠2.5cm，鏈條：皮繩扣環，特殊：附保證書"
+    },
+    {
+        category: "天珠",
+        id: "天珠01",
+        image: "images/dzi01.jpg",
+        specs: "樣式：九眼天珠，尺寸：3.5cm長，鏈條：黑檀木念珠鍊，配飾：綠松石隔片"
+    },
+    {
+        category: "牛角珠",
+        id: "牛角珠01",
+        image: "images/horn01.jpg",
+        specs: "材質：西藏氂牛角，直徑：1.8cm，形狀：不規則圓，孔徑：2mm大孔"
+    }
+];
